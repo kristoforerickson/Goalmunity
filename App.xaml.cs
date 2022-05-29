0edd735b414e06dff4ff0015279a631e052320e5
Kristofor Erickson <kristoforerickson@gmail.com>
2022-05-29 11:12:43 -0400

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Goalmunity;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Goalmunity;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
